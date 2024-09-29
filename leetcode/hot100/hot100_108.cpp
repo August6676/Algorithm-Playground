650d64ac88293048591a35085a9fc8dde99cadc1
August6676 <631333748@qq.com>
2024-09-29 13:06:41 +0800

    递归二叉树

---
+++ leetcode/hot100/hot100_108.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0),left(nullptr),right(nullptr) {}
+    TreeNode(int x) : val(x),left(nullptr),right(nullptr) {}
+    TreeNode(int x, TreeNode* left,TreeNode* right) : val(x),left(left),right(right) {}
+};
+
+TreeNode* buildTree(vector<int>& nums,int left,int right) {
+    if (left > right) return nullptr;
+    int mid = left + (right - left) / 2;
+    TreeNode* node = new TreeNode(nums[mid]);
+    node->left = buildTree(nums,left,mid - 1);
+    node->right = buildTree(nums,mid + 1,right);
+    return node;
+}
+
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+    return buildTree(nums,0,nums.size() - 1);
+}
+
+void inorder(TreeNode* root,vector<int>& ans) {
+    if (root == nullptr) return;
+    inorder(root->left,ans);
+    ans.push_back(root->val);
+    inorder(root->right,ans);
+}
+
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    inorder(root,ans);
+    return ans;
+}
+
+int main () {
+    vector<int> nums = {1,2,3,4,5,6,7};
+    TreeNode* root = sortedArrayToBST(nums);
+    vector<int> ans = inorderTraversal(root);
+    for (int num: ans) {
+        cout << num << " ";
+    }
+}
+
---
+++ leetcode/hot100/hot100_543.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0),left(nullptr),right(nullptr) {}
+    TreeNode(int x) : val(x),left(nullptr),right(nullptr) {}
+    TreeNode(int x, TreeNode* left,TreeNode* right) : val(x),left(left),right(right) {}
+};
+
+int maxDepth(TreeNode* root,int& ans) {
+    // 使用 int& 表示一个整型的引用。引用是一个别名，允许你通过一个变量来访问另一个变量，而不需要创建副本。
+    if (root == nullptr) return 0;
+
+    int leftDepth = maxDepth(root->left,ans);
+    int rightDepth = maxDepth(root->right,ans);
+
+    ans = max(leftDepth+rightDepth,ans); // 深度正好是二叉树的直径 比较最大即可
+
+    return max(leftDepth,rightDepth) + 1; // 就是求二叉树的最大深度
+}
+
+int diameterOfBinaryTree(TreeNode* root) {
+    int ans = 0;
+    maxDepth(root,ans);
+    return ans;
+}
+
+int main() {
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->left = new  TreeNode(4);
+    root->right->right = new  TreeNode(3);
+
+    cout << "二叉树的最大直径为：" << endl;
+    cout << diameterOfBinaryTree(root) << endl;
+}
+
