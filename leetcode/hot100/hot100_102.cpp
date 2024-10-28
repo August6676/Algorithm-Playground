8da3a43518c272914700bf46d16cba517d1131c8
August6676 <631333748@qq.com>
2024-10-28 16:44:42 +0800

    新增二叉树层序遍历

---
+++ leetcode/hot100/hot100_102.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(): val(0),left(nullptr),right(nullptr) {}
+    TreeNode(int x): val(x),left(nullptr),right(nullptr) {}
+    TreeNode(int x,TreeNode* left,TreeNode* right): val(x),left(left),right(right) {}
+};
+
+vector<vector<int>> levelOrder(TreeNode* root) {
+    if (root == nullptr) return {}; // 先处理为空的情况
+    queue<TreeNode*> q;
+    q.push(root);
+    vector<vector<int>> result;
+
+    while (!q.empty()) {
+        vector<int> currLevel;
+        int size = q.size();
+
+        for (int i = 0; i < size; ++i) {
+            TreeNode* node = q.front();
+            q.pop();
+            currLevel.push_back(node->val);
+
+            // 将左右节点加入队列
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
+        result.push_back(currLevel);
+    }
+    return result;
+}
+
+void printResult(const vector<vector<int>>& result) {
+    for (const auto& level: result) {
+        for (int val: level) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    cout << "打印层序遍历结果为：" << endl;
+    printResult(levelOrder(root));
+
+    delete root->left->left;
+    delete root->left->right;
+    delete root->left;
+    delete root->right;
+    return 0;
+}
