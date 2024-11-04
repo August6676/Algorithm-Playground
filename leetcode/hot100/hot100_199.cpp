83f725ee7dac52c10769eda87200314056d5c71a
August6676 <631333748@qq.com>
2024-11-04 14:27:20 +0800

    修改了199，不记得修改了啥

--- leetcode/hot100/hot100_199.cpp
+++ leetcode/hot100/hot100_199.cpp
@@ -1,3 +1,52 @@
-//
-// Created by HP on 2024/10/31.
-//
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode():val(0),left(nullptr),right(nullptr) {}
+    TreeNode(int x): val(x),left(nullptr),right(nullptr) {}
+    TreeNode(int x,TreeNode* left,TreeNode* right): val(x),left(left),right(right) {}
+};
+
+/*
+ * 使用二叉树层次遍历，取出每一层最右侧的数值
+ */
+vector<int> rightSideView(TreeNode* root) {
+    if (root == nullptr) return {};
+    vector<int>ans;
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int current_size = q.size();
+        for (int i = 0; i < current_size;i++) {
+            TreeNode* current = q.front();
+            q.pop();
+            if (i == current_size - 1) ans.push_back(current->val);
+            if (current->left) q.push(current->left);
+            if (current->right) q.push(current->right);
+
+        }
+    }
+    return ans;
+}
+
+int main() {
+    TreeNode* root = new TreeNode(0);
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(2);
+    root->left->right = new TreeNode(1);
+    root->right->left = new TreeNode(8);
+    root->right->left->left = new TreeNode(9);
+
+    vector<int> ans = rightSideView(root);
+    cout << "二叉树右视图的结果为：" << "\n";
+    for (int num : ans) {
+        cout << num << " " << endl;
+    }
+}
