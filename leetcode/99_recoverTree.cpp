ea58000d1e335f7a42fbbfee6ad84fb1f8ffdf78
August6676 <631333748@qq.com>
2024-11-08 10:16:33 +0800

    新增一道深度优先，一道中序遍历（一到递归就有点难想）

---
+++ leetcode/99_recoverTree.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/*
+ * 自己做的时候执行了一整遍中序遍历，然后再去排序val再去更改 可以使用两个指针记录的方式简化
+ * 但是又涉及到& 也就是都使用同一个指针
+ * 还是稍微有点难想
+ */
+void InorderTraversal(TreeNode* root,TreeNode*& prev,TreeNode*& first,TreeNode*& second) {
+    if (root == nullptr) return;
+    InorderTraversal(root->left,prev,first,second);
+
+    // 检查是否逆序
+    if (prev && root->val < prev->val) {
+        if (!first) first = prev;
+        second = root;
+    }
+
+    prev = root;
+    InorderTraversal(root->right,prev,first,second);
+}
+
+void recoverTree(TreeNode* root) {
+    TreeNode* first = nullptr;
+    TreeNode* second = nullptr;
+    TreeNode* prev = nullptr;
+
+    InorderTraversal(root,prev,first,second);
+
+    if (first && second) {
+        swap(first->val,second->val);
+    }
+}
