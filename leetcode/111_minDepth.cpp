612c073e03d2532b73e2e5921894d0993d67b0ee
August6676 <631333748@qq.com>
2024-11-09 10:12:42 +0800

    求二叉树的最小深度，vs求最大深度

---
+++ leetcode/111_minDepth.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/*
+ * vs https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/ 二叉树的最大深度
+ * https://github.com/August6676/Algorithm-Playground/blob/master/leetcode/hot100/hot100_104.cpp
+ * 求最小深度 要注意 如果有一边子树为空，也不可以认为是0
+ * 自己做的又是多次重复递归 优化后及时止损
+ */
+int minDepth(TreeNode* root) {
+    if (root == nullptr) return 0; // 节点为空
+    if (root->left == nullptr && root->right == nullptr) return 1; // 节点为叶子节点
+
+    // 左右子树如果不为空 那么就计算深度 否则就取无穷大
+    int leftDepth = root->left ? minDepth(root->left): INT_MAX;
+    int rightDepth = root->right ? minDepth(root->right): INT_MAX;
+
+    // 加上自己的1
+    return min(leftDepth,rightDepth) + 1;
+}
