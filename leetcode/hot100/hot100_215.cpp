b3f658ea064b2d36517c18cc64c021600564c710
August6676 <631333748@qq.com>
2024-12-06 19:13:12 +0800

    先使用快速排序的方法，不过还是会超时，因此有三路快排

---
+++ leetcode/hot100/hot100_215.cpp
@@ -0,0 +1,67 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+/*
+ * 这里使用快速排序，实现找到第k大的元素
+ * 记模板
+ */
+int partition1(vector<int>& nums, int left, int right) {
+    int pivot = nums[right]; // 选择最后一个元素作为基准
+    int i = left; // i存放比pivot小的区域的右边界
+
+    for (int j = left; j < right; ++j) {
+        if (nums[j] <= pivot) {
+            swap(nums[i],nums[j]);
+            i++;
+        }
+    }
+
+    // 最后交换到正确的位置
+    swap(nums[i],nums[right]);
+    return i;
+}
+
+int quickSelect1(vector<int>& nums, int left, int right, int k) {
+    // 在这里 k已经做过减法了 现在也就是找从左到右第k大的数了
+    // 选择区分点
+    int partitionIndex = partition1(nums,left,right);
+
+    if (partitionIndex == k) return nums[partitionIndex];
+
+    if (partitionIndex < k) return quickSelect1(nums,partitionIndex + 1,right,k);
+    return quickSelect1(nums,left,partitionIndex - 1,k);
+}
+
+// 输入数组中有大量重复元素
+// 三路归并排序 中间是一个区间
+pair<int,int> partition(vector<int>& nums, int left, int right) {
+    int pivot = nums[right];
+    int lt = left, i = left, gt = right;
+    while (i <= gt) {
+        if (nums[i] < pivot) swap(nums[lt++],nums[i++]);
+        else if (nums[i] > pivot) swap(nums[i],nums[gt--]);
+        else i++;
+    }
+    return {lt,gt};
+}
+
+int quickSelect(vector<int>& nums, int left, int right, int k) {
+    auto[lt,gt] = partition(nums,left,right);
+    if (k >= lt && k <= gt) return nums[k];
+    if (k < lt) return quickSelect(nums,left,lt - 1,k);
+    else return quickSelect(nums,gt + 1,right,k);
+}
+
+
+int findKthLargest(vector<int>& nums, int k) {
+    return quickSelect1(nums,0,nums.size() - 1,nums.size() - k);
+}
+
+int main() {
+    vector<int> nums = {3,2,1,5,6,4};
+    int k = 2;
+    cout << "第 " << k << " 大的元素是 " << findKthLargest(nums,k);
+}
